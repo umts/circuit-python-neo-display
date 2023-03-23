42302e764462ee37acfc634672bde02bb6dc5eb8
jforgue <26842354+bandogora@users.noreply.github.com>
2023-03-23 17:08:59 -0400

    UART read and display write

--- code.py
+++ code.py
@@ -1,20 +1,52 @@
 import board
+import busio
 from adafruit_neopxl8 import NeoPxl8
 from adafruit_pixel_framebuf import PixelFramebuffer
+from adafruit_led_animation.helper import PixelMap
 
-first_led_pin = board.NEOPIXEL0
+UART = busio.UART(None, board.RX, baudrate=1000000, parity="odd", timeout=1, stop=1)
+NUM_DISPLAYS = 6
+STRAND_LENGTH = 128
+NUM_PIXELS = NUM_DISPLAYS * STRAND_LENGTH
 
-pixels = NeoPxl8(
-  first_led_pin,
-  128,
-  num_strands=1,
-  auto_write=True,
-  brightness=0.05,
+PIXELS = NeoPxl8(
+    board.NEOPIXEL0,
+    NUM_PIXELS,
+    num_strands=NUM_DISPLAYS,
+    auto_write=False,
+    brightness=0.02,
 )
 
-pixel_framebuf = PixelFramebuffer(pixels, 8, 16, alternating=False, rotation=3)
-# pixel_framebuf.stride = 16
+def map_pixels(display_num):
+  return PixelMap(
+    PIXELS,
+    range(display_num * STRAND_LENGTH, (display_num + 1) * STRAND_LENGTH),
+    individual_pixels=True,
+  )
 
-pixel_framebuf.text("43", 2, 0, 0x00467E)
-# pixel_framebuf.fill(0x00467E)
-pixel_framebuf.display()
+def frame_buf(display):
+  return PixelFramebuffer(display, 8, 16, alternating=False, rotation=3)
+
+def data_frame_to_int(byte):
+  return int.from_bytes(recv_buf[byte:byte+2], 'big')
+
+displays = [map_pixels(i) for i in range(NUM_DISPLAYS)]
+frame_bufs = [frame_buf(displays[i]) for i in range(NUM_DISPLAYS)]
+
+while True:
+  recv_buf = UART.read(NUM_DISPLAYS * 2)
+
+  if recv_buf is not None:
+    data_frames = [data_frame_to_int(byte) for byte in range(0, NUM_DISPLAYS * 2, 2)]
+
+    for i, data_frame in enumerate(data_frames):
+      print(str(i), '->', str(data_frame))
+      if data_frame < 10:
+        frame_bufs[i].text(str(data_frame), 5, 0, 0x00467E)
+      elif data_frame < 100:
+        frame_bufs[i].text(str(data_frame), 2, 0, 0x00467E)
+      else:
+        frame_bufs[i].text(str(data_frame), 0, 0, 0x00467E)
+      frame_bufs[i].display()
+
+PIXELS.deinit()
